99b6e40ba2d59bbd14c4524f626630022d45b884
yahya taouarda <yahyataouarda@gmail.com>
2025-08-13 10:40:53 +0100

    save

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,23 @@
 const header = document.querySelector("header");
 const navBar = document.querySelector("nav");
+const navLinks = document.querySelector("nav a");
+const links = Array.from(document.querySelectorAll("a")).filter(
+  (link) => links.getAttribute("href")[0] === "#"
+);
+
+const section = Array.from(navLinks).map((link) =>
+  document.querySelector(link.getAttribute("href"))
+);
+links.forEach((link) => {
+  link.onclick = (e) => {
+    e.preventDefault();
+    navBar.classList.remove("active");
+    const section = document.querySelector(link.getAttribute("href"));
+    if (section) {
+      window.scrollTo({
+        top: section.offsetTop - 50,
+        behavior: "smooth",
+      });
+    }
+  };
+});
